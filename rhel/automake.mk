gvrose8192/ovs-temp

commit b9855888da24c020d76f3a4edf940d197f56172d

rhel: Add a suse spec file.
This is just a copy of openvswitch.spec.in. In addition,
it just adds systemd unit files to start openvswitch.
The systemd files are the same as used by rhel/fedora.

Signed-off-by: Gurucharan Shetty <guru@ovn.org>

rhel/automake.mk
@@ -23,6 +23,8 @@ EXTRA_DIST += \
 	rhel/openvswitch.spec.in \
 	rhel/openvswitch-fedora.spec \
 	rhel/openvswitch-fedora.spec.in \
+	rhel/openvswitch-suse.spec \
+	rhel/openvswitch-suse.spec.in \
 	rhel/usr_share_openvswitch_scripts_ovs-systemd-reload \
 	rhel/usr_share_openvswitch_scripts_sysconfig.template \
 	rhel/usr_share_openvswitch_scripts_systemd_sysconfig.template \

rhel/.gitignore
@@ -4,4 +4,5 @@ kmod-openvswitch-rhel6.spec
 openvswitch-kmod-fedora.spec
 openvswitch.spec
 openvswitch-fedora.spec
+openvswitch-suse.spec
 usr_lib_systemd_system_ovs-vswitchd.service

rhel/openvswitch-suse.spec.in
@@ -1,0 +1,283 @@
+# Spec file for Open vSwitch on SUSE Linux.
+# Copyright (C) 2019 VMware, Inc.
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without warranty of any kind.
+#
+# If tests have to be skipped while building, specify the '--without check'
+# option. For example:
+#     rpmbuild -bb --without check rhel/openvswitch-suse.spec
+#
+
+Name: openvswitch
+Summary: Open vSwitch daemon/database/utilities
+Group: System Environment/Daemons
+URL: http://www.openvswitch.org/
+Vendor: Nicira, Inc.
+Version: @VERSION@
+
+License: ASL 2.0
+Release: 1
+Source: openvswitch-%{version}.tar.gz
+Buildroot: /tmp/openvswitch-rpm
+Requires: logrotate, net-tools, python >= 2.7, python-six
+BuildRequires: python-six
+BuildRequires: openssl-devel
+BuildRequires: checkpolicy, selinux-policy-devel
+BuildRequires: autoconf, automake, libtool
+BuildRequires: python-sphinx
+
+%bcond_without check
+%bcond_with check_datapath_kernel
+
+%description
+Open vSwitch provides standard network bridging functions and
+support for the OpenFlow protocol for remote per-flow control of
+traffic.
+
+%package devel
+Summary:        Open vSwitch development package
+Group:          Development/Libraries
+
+%description devel
+This package provides openvswitch headers and libopenvswitch for developers.
+
+%package selinux-policy
+Summary: Open vSwitch SELinux policy
+License: ASL 2.0
+BuildArch: noarch
+Requires: selinux-policy-targeted
+
+%description selinux-policy
+Tailored Open vSwitch SELinux policy
+
+%prep
+%setup -q
+
+%build
+./configure --prefix=/usr --sysconfdir=/etc --localstatedir=%{_localstatedir} \
+    --libdir=%{_libdir} --enable-ssl --enable-shared
+build-aux/dpdkstrip.py --nodpdk < rhel/usr_lib_systemd_system_ovs-vswitchd.service.in \
+      > rhel/usr_lib_systemd_system_ovs-vswitchd.service
+make %{_smp_mflags}
+make selinux-policy
+
+%install
+rm -rf $RPM_BUILD_ROOT
+make install DESTDIR=$RPM_BUILD_ROOT
+
+rhel_cp() {
+ base=$1
+ mode=$2
+ dst=$RPM_BUILD_ROOT/$(echo $base | sed 's,_,/,g')
+ install -D -m $mode rhel/$base $dst
+}
+rhel_cp etc_logrotate.d_openvswitch 0644
+rhel_cp etc_sysconfig_network-scripts_ifup-ovs 0755
+rhel_cp etc_sysconfig_network-scripts_ifdown-ovs 0755
+
+install -p -D -m 0644 \
+        rhel/usr_share_openvswitch_scripts_systemd_sysconfig.template \
+        $RPM_BUILD_ROOT/%{_sysconfdir}/sysconfig/openvswitch
+for service in openvswitch ovsdb-server ovs-vswitchd; do
+        install -p -D -m 0644 \
+                        rhel/usr_lib_systemd_system_${service}.service \
+                        $RPM_BUILD_ROOT%{_unitdir}/${service}.service
+done
+install -m 0755 rhel/etc_init.d_openvswitch \
+        $RPM_BUILD_ROOT%{_datadir}/openvswitch/scripts/openvswitch.init
+install -p -D -m 0644 rhel/etc_openvswitch_default.conf \
+        $RPM_BUILD_ROOT/%{_sysconfdir}/openvswitch/default.conf
+install -p -D -m 0755 \
+        rhel/usr_share_openvswitch_scripts_ovs-systemd-reload \
+        $RPM_BUILD_ROOT%{_datadir}/openvswitch/scripts/ovs-systemd-reload
+
+install -p -m 644 -D selinux/openvswitch-custom.pp \
+    $RPM_BUILD_ROOT%{_datadir}/selinux/packages/%{name}/openvswitch-custom.pp
+
+# Get rid of stuff we don't want to make RPM happy.
+rm \
+    $RPM_BUILD_ROOT/usr/bin/ovs-testcontroller \
+    $RPM_BUILD_ROOT/usr/share/man/man8/ovs-testcontroller.8 \
+    $RPM_BUILD_ROOT/usr/bin/ovs-test \
+    $RPM_BUILD_ROOT/usr/bin/ovs-l3ping \
+    $RPM_BUILD_ROOT/usr/share/man/man8/ovs-test.8 \
+    $RPM_BUILD_ROOT/usr/share/man/man8/ovs-l3ping.8 \
+    $RPM_BUILD_ROOT/usr/sbin/ovs-vlan-bug-workaround \
+    $RPM_BUILD_ROOT/usr/share/man/man8/ovs-vlan-bug-workaround.8 \
+    $RPM_BUILD_ROOT/usr/bin/ovn-* \
+    $RPM_BUILD_ROOT/usr/share/man/man?/ovn-* \
+    $RPM_BUILD_ROOT/usr/share/openvswitch/ovn-* \
+    $RPM_BUILD_ROOT/usr/share/openvswitch/scripts/ovn*
+(cd "$RPM_BUILD_ROOT" && rm -rf usr/%{_lib}/*.la)
+(cd "$RPM_BUILD_ROOT" && rm -rf usr/include)
+
+install -d -m 0755 $RPM_BUILD_ROOT%{_rundir}/openvswitch
+install -d -m 0755 $RPM_BUILD_ROOT%{_localstatedir}/log/openvswitch
+install -d -m 0755 $RPM_BUILD_ROOT/var/lib/openvswitch
+
+copy_headers() {
+    src=$1
+    dst=$RPM_BUILD_ROOT/$2
+    install -d -m 0755 $dst
+    install -m 0644 $src/*.h $dst
+}
+copy_headers include %{_includedir}/openvswitch
+copy_headers include/openflow %{_includedir}/openvswitch/openflow
+copy_headers include/openvswitch %{_includedir}/openvswitch/openvswitch
+copy_headers include/sparse %{_includedir}/openvswitch/sparse
+copy_headers include/sparse/arpa %{_includedir}/openvswitch/sparse/arpa
+copy_headers include/sparse/netinet %{_includedir}/openvswitch/sparse/netinet
+copy_headers include/sparse/sys %{_includedir}/openvswitch/sparse/sys
+copy_headers lib %{_includedir}/openvswitch/lib
+
+install -D -m 0644 lib/.libs/libopenvswitch.a \
+    $RPM_BUILD_ROOT/%{_libdir}/libopenvswitch.a
+
+%check
+%if %{with check}
+    if make check TESTSUITEFLAGS='%{_smp_mflags}' RECHECK=yes; then :;
+    else
+        cat tests/testsuite.log
+        exit 1
+    fi
+%endif
+%if %{with check_datapath_kernel}
+    if make check-kernel RECHECK=yes; then :;
+    else
+        cat tests/system-kmod-testsuite.log
+        exit 1
+    fi
+%endif
+
+%clean
+rm -rf $RPM_BUILD_ROOT
+
+%post
+systemctl daemon-reload
+systemctl enable openvswitch
+if [ -e "%{_tmppath}/ovs-upgrade-from-sysv" ]; then
+    systemctl restart openvswitch
+    rm "%{_tmppath}/ovs-upgrade-from-sysv"
+fi
+
+%pre selinux-policy
+%selinux_relabel_pre -s targeted
+if [ -x "/etc/init.d/openvswitch" ]; then
+    touch %{_tmppath}/ovs-upgrade-from-sysv
+fi
+
+%post selinux-policy
+%selinux_modules_install -s targeted %{_datadir}/selinux/packages/%{name}/openvswitch-custom.pp
+
+%preun
+if [ "$1" = "0" ]; then     # $1 = 0 for uninstall
+    /bin/systemctl --no-reload disable %{name}.service >/dev/null 2>&1 || :
+    /bin/systemctl stop %{name}.service >/dev/null 2>&1 || :
+fi
+
+%postun
+if [ "$1" = "0" ]; then     # $1 = 0 for uninstall
+    /bin/systemctl daemon-reload >/dev/null 2>&1 || :
+    rm -f /etc/openvswitch/conf.db
+    rm -f /etc/openvswitch/vswitchd.cacert
+fi
+
+%postun selinux-policy
+if [ $1 -eq 0 ] ; then
+  %selinux_modules_uninstall -s targeted openvswitch-custom
+fi
+
+exit 0
+
+%posttrans selinux-policy
+%selinux_relabel_post -s targeted
+
+%files
+%defattr(-,root,root)
+%dir /etc/openvswitch
+/etc/bash_completion.d/ovs-appctl-bashcomp.bash
+/etc/bash_completion.d/ovs-vsctl-bashcomp.bash
+%config(noreplace) /etc/logrotate.d/openvswitch
+/etc/sysconfig/network-scripts/ifup-ovs
+/etc/sysconfig/network-scripts/ifdown-ovs
+%{_unitdir}/openvswitch.service
+%{_unitdir}/ovsdb-server.service
+%{_unitdir}/ovs-vswitchd.service
+%{_datadir}/openvswitch/scripts/openvswitch.init
+%config(noreplace) %{_sysconfdir}/sysconfig/openvswitch
+%{_sysconfdir}/openvswitch/default.conf
+/usr/bin/ovs-appctl
+/usr/bin/ovs-dpctl
+/usr/bin/ovs-dpctl-top
+/usr/bin/ovs-docker
+/usr/bin/ovs-ofctl
+/usr/bin/ovs-parse-backtrace
+/usr/bin/ovs-pcap
+/usr/bin/ovs-pki
+/usr/bin/ovs-tcpdump
+/usr/bin/ovs-tcpundump
+/usr/bin/ovs-vlan-test
+/usr/bin/ovs-vsctl
+/usr/bin/ovsdb-client
+/usr/bin/ovsdb-tool
+/usr/bin/vtep-ctl
+%{_libdir}/lib*.so.*
+/usr/sbin/ovs-bugtool
+/usr/sbin/ovs-vswitchd
+/usr/sbin/ovsdb-server
+/usr/share/man/man1/ovs-pcap.1.gz
+/usr/share/man/man1/ovs-tcpundump.1.gz
+/usr/share/man/man1/ovsdb-client.1.gz
+/usr/share/man/man1/ovsdb-server.1.gz
+/usr/share/man/man1/ovsdb-tool.1.gz
+/usr/share/man/man5/ovsdb-server.5.gz
+/usr/share/man/man5/ovs-vswitchd.conf.db.5.gz
+%{_mandir}/man5/ovsdb.5*
+/usr/share/man/man5/vtep.5.gz
+/usr/share/man/man7/ovs-actions.7.gz
+/usr/share/man/man7/ovs-fields.7.gz
+%{_mandir}/man7/ovsdb.7*
+%{_mandir}/man7/ovsdb-server.7*
+/usr/share/man/man8/ovs-appctl.8.gz
+/usr/share/man/man8/ovs-bugtool.8.gz
+/usr/share/man/man8/ovs-ctl.8.gz
+/usr/share/man/man8/ovs-dpctl.8.gz
+/usr/share/man/man8/ovs-dpctl-top.8.gz
+/usr/share/man/man8/ovs-kmod-ctl.8.gz
+/usr/share/man/man8/ovs-ofctl.8.gz
+/usr/share/man/man8/ovs-parse-backtrace.8.gz
+/usr/share/man/man8/ovs-pki.8.gz
+/usr/share/man/man8/ovs-tcpdump.8.gz
+/usr/share/man/man8/ovs-vlan-test.8.gz
+/usr/share/man/man8/ovs-vsctl.8.gz
+/usr/share/man/man8/ovs-vswitchd.8.gz
+/usr/share/man/man8/vtep-ctl.8.gz
+/usr/share/openvswitch/bugtool-plugins/
+/usr/share/openvswitch/python/
+/usr/share/openvswitch/scripts/ovs-bugtool-*
+/usr/share/openvswitch/scripts/ovs-check-dead-ifs
+/usr/share/openvswitch/scripts/ovs-ctl
+/usr/share/openvswitch/scripts/ovs-kmod-ctl
+/usr/share/openvswitch/scripts/ovs-lib
+/usr/share/openvswitch/scripts/ovs-save
+/usr/share/openvswitch/scripts/ovs-vtep
+/usr/share/openvswitch/scripts/ovs-monitor-ipsec
+/usr/share/openvswitch/scripts/ovs-systemd-reload
+/usr/share/openvswitch/vswitch.ovsschema
+/usr/share/openvswitch/vtep.ovsschema
+%doc NOTICE README.rst NEWS rhel/README.RHEL.rst
+/var/lib/openvswitch
+/var/log/openvswitch
+
+%files devel
+%{_libdir}/lib*.so
+%{_libdir}/lib*.a
+%{_libdir}/pkgconfig
+%{_includedir}/openvswitch/*
+
+%files selinux-policy
+%defattr(-,root,root)
+%{_datadir}/selinux/packages/%{name}/openvswitch-custom.pp

TomCodeLV/ovs

commit be4f0a43db191af9c19709e8a06e2d0536bc23c2

rhel: Fix RHEL build issues on impactor
Change b9855888da24 ("rhel: add a suse spec file") introduced .spec
files to build SUSE/SLES rpms.  However, the change did not also include
code to call the 'update_rhel_spec' and as such the target was not
defined correctly.  Later, code that runs dpdkstrip.py on a number of
utilities and scripts also executes on the rhel/openvswitch-suse.spec.in
and causes a build error:

/bin/sh: line 3: rhel/openvswitch-suse.spec.tmp: No such file or directory
Traceback (most recent call last):
  File "../build-aux/dpdkstrip.py", line 48, in <module>
    strip_dpdk(check_dpdk, sys.stdin, sys.stdout)
  File "../build-aux/dpdkstrip.py", line 31, in strip_dpdk
    dst.write(line)

One other error occurs because of requirements for a build id.  Add
line %undefine _missing_build_ids_terminate_build to  the fedora
spec file to change the errors into warnings so that we can complete
desktop builds

Fixes:  b9855888da24 ("rhel: add a suse spec file")
Signed-off-by: Greg Rose <roseg@vmware.com>

rhel/automake.mk
@@ -58,6 +58,9 @@ $(srcdir)/rhel/openvswitch.spec: rhel/openvswitch.spec.in $(top_builddir)/config
 $(srcdir)/rhel/openvswitch-fedora.spec: rhel/openvswitch-fedora.spec.in $(top_builddir)/config.status
 	$(update_rhel_spec)
 
+$(srcdir)/rhel/openvswitch-suse.spec: rhel/openvswitch-suse.spec.in $(top_builddir)/config.status
+	$(update_rhel_spec)
+
 RPMBUILD_TOP := $(abs_top_builddir)/rpm/rpmbuild
 RPMBUILD_OPT ?= --without check
 

rhel/openvswitch-fedora.spec.in
@@ -39,6 +39,8 @@
 # specify the "--with autoenable"
 %bcond_with autoenable
 
+%undefine _missing_build_ids_terminate_build
+
 # Enable PIE, bz#955181
 %global _hardened_build 1
 